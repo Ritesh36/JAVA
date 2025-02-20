107b587dfbce6361560c67237613dfc0a6d48427
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-02-20 23:57:14 +0530

    new file:   ArrayCC.java

---
+++ ArrayCC.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ArrayCC {
+    public static void updateArray(int marks[]){
+        for(int i=0; i<marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
+    }
+    public static void main(String args[]){
+        int marks[] = {97, 98, 99};
+
+        System.out.println("Before updateArray: ");
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]);
+        }
+
+        updateArray(marks);
+        
+        System.out.println("After updateArray: ");
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
