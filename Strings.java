4a3413eaedeeaed044da90319c1920eb18666078
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-03-25 16:08:30 +0530

    solved more string questions

--- Strings.java
+++ Strings.java
@@ -55,16 +55,56 @@ public class Strings {
 
         return (float)Math.sqrt(X2 + Y2);
     }
-    public static void main(String[] args) {
-       String fruits[] = {"apple", "banana", "mango"};
 
-       String largest = fruits[0];
+    public static String toUpperCase(String str) {
 
-       for(int i=1; i<fruits.length; i++){
-        if(largest.compareTo(fruits[i]) < 0){
-            largest = fruits[i];
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=1; i<str.length(); i++){
+            if(str.charAt(i) == ' ' && i<str.length() - 1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
         }
-       }
-       System.out.println(largest);
+
+        return sb.toString();
+    }
+
+    public static String stringCompress(String str) {
+        StringBuilder sb = new StringBuilder("");
+        
+        for(int i=0; i<str.length(); i++){
+            Integer count = 1;
+            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count > 1) {
+                sb.append(count.toString());
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+    //    String fruits[] = {"apple", "banana", "mango"};
+
+    //    String largest = fruits[0];
+
+    //    for(int i=1; i<fruits.length; i++){
+    //     if(largest.compareTo(fruits[i]) < 0){
+    //         largest = fruits[i];
+    //     }
+    //    }
+    //    System.out.println(largest);
+
+    String str = "aaabbbcd";
+    System.out.println(stringCompress(str));
     }
 }
