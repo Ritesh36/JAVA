80bf22ff524a498a0e8df161f147221c435c41da
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-03-30 20:58:11 +0530

    started learning OOPs concepts

--- Practice.java
+++ Practice.java
@@ -103,6 +103,8 @@ class Practice {
             }
             return tappedWater;
         }
+
+        
         public static void main(String args[]){
             int height[] = {1,8,6,2,5,4,8,3,7};
             System.out.println(maxArea(height));
