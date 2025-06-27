c06c3a324a7c2a7d52d9cd735cf14af0a0b2722f
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-06-28 00:06:15 +0530

    solved more questions on queue

--- QueueB.java
+++ QueueB.java
@@ -2,74 +2,72 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class QueueB {
-    static class MyStack {
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
 
-        public boolean isEmpty() {
-            return q1.isEmpty() && q2.isEmpty();
-        }
+    public static void printNonRepeating(String str) {
+        int freq[] = new int[26];
+        Queue<Character> q = new LinkedList<>();
 
-        public void push(int data) {
-            if (!q1.isEmpty()) {
-                q1.add(data);
-            } else {
-                q2.add(data);
-            }
-        }
+        for(int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            freq[ch - 'a']++;
+            q.add(ch);
 
-        public int pop() {
-            if (isEmpty()) {
-                System.out.println("Stack is empty");
-                return -1;
+            while(!q.isEmpty() && freq[q.peek()-'a'] > 1) {
+                q.remove();
             }
-            int top = -1;
-            if (!q1.isEmpty()) {
-                while (!q1.isEmpty()) {
-                    top = q1.remove();
-                    if (q1.isEmpty()) break;
-                    q2.add(top);
-                }
-            } else {
-                while (!q2.isEmpty()) {
-                    top = q2.remove();
-                    if (q2.isEmpty()) break;
-                    q1.add(top);
-                }
-            }
-            return top;
-        }
 
-        public int peek() {
-            if (isEmpty()) {
-                System.out.println("Stack is empty");
-                return -1;
+            if(q.isEmpty()) {
+                System.out.print("-1"+ " ");
+            }else {
+                System.out.print(q.peek()+ " ");
             }
-            int top = -1;
-            if (!q1.isEmpty()) {
-                while (!q1.isEmpty()) {
-                    top = q1.remove();
-                    q2.add(top);
-                }
-            } else {
-                while (!q2.isEmpty()) {
-                    top = q2.remove();
-                    q1.add(top);
-                }
-            }
-            return top;
         }
+        System.out.println();
     }
 
-    public static void main(String[] args) {
-        MyStack s = new MyStack();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+    public static void interLeave(Queue<Integer> q) {
+        Queue<Integer> firstHalf = new LinkedList<>();
+        int size = q.size()/2;
 
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
+        for(int i=0; i<size; i++) {
+            firstHalf.add(q.remove());
         }
+
+        while(!firstHalf.isEmpty()) {
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+        }
+    }
+    public static void main(String[] args) {
+        // MyStack s = new MyStack();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+        // while (!s.isEmpty()) {
+        //     System.out.println(s.peek());
+        //     s.pop();
+        // }
+        // String str = "aabccxb";
+        // printNonRepeating(str);
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+
+        interLeave(q);
+
+        while(!q.isEmpty()) {
+            System.out.print(q.remove()+" ");
+        }
+        System.out.println();
     }
 }
