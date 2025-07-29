9fdb5238ab1d2fc2c57a528da072a6f422f8ba1e
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-07-29 15:42:24 +0530

    refactor ArrayCC.java for improved code readability and formatting; add methods for pair sum, triplets, unique elements, and second largest

--- ArrayCC.java
+++ ArrayCC.java
@@ -1,68 +1,68 @@
 import java.util.*;
 
 public class ArrayCC {
-    public static int linearSearch(int numbers[], int key){
-        for(int i=0; i<=numbers.length; i++){
-            if(numbers[i] == key){
+    public static int linearSearch(int numbers[], int key) {
+        for (int i = 0; i <= numbers.length; i++) {
+            if (numbers[i] == key) {
                 return i;
             }
         }
         return -1;
     }
 
-    public static int getLargest(int numbers[]){
+    public static int getLargest(int numbers[]) {
         int largest = Integer.MIN_VALUE;
-        for(int i=0; i<numbers.length; i++){
-            if(largest < numbers[i]){
+        for (int i = 0; i < numbers.length; i++) {
+            if (largest < numbers[i]) {
                 largest = numbers[i];
             }
         }
         return largest;
     }
 
-    public static int getSmallest(int numbers[]){
+    public static int getSmallest(int numbers[]) {
         int smallest = Integer.MAX_VALUE;
-        for(int i=0; i<numbers.length; i++){
-            if(smallest > numbers[i]){
+        for (int i = 0; i < numbers.length; i++) {
+            if (smallest > numbers[i]) {
                 smallest = numbers[i];
             }
         }
         return smallest;
     }
 
-    public static int binarySearch(int numbers[], int key){//needs sorted array
+    public static int binarySearch(int numbers[], int key) {// needs sorted array
         int start = 0;
         int end = numbers.length;
         int mid;
 
-        mid = (start+end)/2;
+        mid = (start + end) / 2;
 
-        while(start <= end){
+        while (start <= end) {
 
-           if(numbers[mid] == key){
+            if (numbers[mid] == key) {
                 return mid;
-           } else if(numbers[mid] < key){ // key is in right side
+            } else if (numbers[mid] < key) { // key is in right side
                 start = mid + 1;
-           } else {     // key is in left side
+            } else { // key is in left side
                 end = mid - 1;
-           }
+            }
         }
         return -1;
     }
 
-    public static void reverseArr(int numbers[]){
+    public static void reverseArr(int numbers[]) {
         // int reverseArr[] = new int[numbers.length];
         // int j=0;
         // for(int i=numbers.length-1; i>=0; i--){
-        //     reverseArr[j++] = numbers[i];
+        // reverseArr[j++] = numbers[i];
         // }
         // for(int i=0; i<reverseArr.length; i++){
-        //     System.out.println(reverseArr[i]);
+        // System.out.println(reverseArr[i]);
         // }
         int first = 0;
         int last = numbers.length - 1;
 
-        while (first < last){
+        while (first < last) {
 
             int temp = numbers[last];
             numbers[last] = numbers[first];
@@ -73,11 +73,11 @@ public class ArrayCC {
         }
     }
 
-    public static void pairArr(int numbers[]){
+    public static void pairArr(int numbers[]) {
         int tp = 0;
-        for(int i=0; i<numbers.length; i++){
-            for(int j=i+1; j<numbers.length; j++){
-                System.out.print("("+numbers[i]+", "+numbers[j]+") ");
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                System.out.print("(" + numbers[i] + ", " + numbers[j] + ") ");
                 tp++;
             }
             System.out.println();
@@ -85,29 +85,29 @@ public class ArrayCC {
         System.out.println("Total Nos of pairs = " + tp);
     }
 
-    public static void subArray(int numbers[]){
-       
-        for(int i=0; i<numbers.length; i++){
+    public static void subArray(int numbers[]) {
+
+        for (int i = 0; i < numbers.length; i++) {
             int start = i;
-            for(int k=i;k<numbers.length;k++){
+            for (int k = i; k < numbers.length; k++) {
                 int end = k;
-                for(int j=start; j<=end; j++){
-                    System.out.print(numbers[j]+" ");
+                for (int j = start; j <= end; j++) {
+                    System.out.print(numbers[j] + " ");
                 }
                 System.out.println();
-            } 
-            System.out.println();   
+            }
+            System.out.println();
         }
     }
 
-    public static void maxSubArraySum(int numbers[]){
-        //Most Optimal Solution(kadanes algorithm)
+    public static void maxSubArraySum(int numbers[]) {
+        // Most Optimal Solution(kadanes algorithm)
         int maxSum = Integer.MIN_VALUE;
         int cs = 0;
 
-        for(int i=0; i<numbers.length; i++){
+        for (int i = 0; i < numbers.length; i++) {
             cs = cs + numbers[i];
-            if(cs < 0){
+            if (cs < 0) {
                 cs = 0;
             }
             maxSum = Math.max(maxSum, cs);
@@ -115,21 +115,21 @@ public class ArrayCC {
         System.out.println("Max Sum : " + maxSum);
     }
 
-    public static int trappedRainWater(int height[]){
+    public static int trappedRainWater(int height[]) {
         int leftMax[] = new int[height.length];
         leftMax[0] = height[0];
-        for(int i=1; i<height.length; i++){
-            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
 
         int rightMax[] = new int[height.length];
-        rightMax[height.length-1] = height[height.length-1];
-        for(int i=height.length - 2; i>=0; i--){
-            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        rightMax[height.length - 1] = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
         }
 
         int trappedWater = 0;
-        for(int i=0; i<height.length; i++){
+        for (int i = 0; i < height.length; i++) {
             int waterLevel = Math.min(leftMax[i], rightMax[i]);
             // trappedWater += waterLevel - height[i];
             trappedWater = trappedWater + (waterLevel - height[i]);
@@ -137,7 +137,6 @@ public class ArrayCC {
 
         return trappedWater;
     }
-
 
     public static int maxProfit(int[] prices) {
         if (prices == null || prices.length <= 1) {
@@ -166,14 +165,14 @@ public class ArrayCC {
         int end = nums.length - 1;
         int currSum = 0;
         int count = 0;
-    
-        while(start < end){
+
+        while (start < end) {
             currSum = nums[start] + nums[end];
-            if(currSum == target){
+            if (currSum == target) {
                 count++;
                 start++;
-                end --;
-            } else if(currSum < target){
+                end--;
+            } else if (currSum < target) {
                 start++;
             } else {
                 end--;
@@ -184,14 +183,14 @@ public class ArrayCC {
 
     public static int majorityElement(int[] nums) {
         int n = nums.length;
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int count = 0;
-            for(int j=0; j<n; j++){
-                if(nums[i] == nums[j]){
+            for (int j = 0; j < n; j++) {
+                if (nums[i] == nums[j]) {
                     count++;
                 }
             }
-            if(count > n/2){
+            if (count > n / 2) {
                 return nums[i];
             }
         }
@@ -201,8 +200,8 @@ public class ArrayCC {
     public static int missingNumber(int nums[]) {
         Arrays.sort(nums);
 
-        for(int i=0; i<nums.length-1; i++) {
-           if(nums[i+1] == nums[i] + 1) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] == nums[i] + 1) {
                 continue;
             } else {
                 return nums[i] + 1;
@@ -222,10 +221,10 @@ public class ArrayCC {
         }
     }
 
-    //Prefix Sum
+    // Prefix Sum
     public static int[] prefixSum(int arr[]) {
-        for(int i=1; i<arr.length; i++) {
-            arr[i] += arr[i-1];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] += arr[i - 1];
         }
         return arr;
     }
@@ -242,7 +241,7 @@ public class ArrayCC {
 
     public static int totalSum(int arr[]) {
         int totalSum = 0;
-        for(int i=0; i<arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             totalSum += arr[i];
         }
         return totalSum;
@@ -250,18 +249,77 @@ public class ArrayCC {
 
     public static boolean subarrayPartition(int arr[]) {
         int totalSum = totalSum(arr);
-        for(int i=1; i<arr.length; i++) {
-            arr[i] += arr[i-1];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] += arr[i - 1];
             int sufixSum = totalSum - arr[i];
-            if(arr[i] == sufixSum) {
+            if (arr[i] == sufixSum) {
                 return true;
             }
         }
         return false;
     }
-    
 
-    public static void main(String args[]){
+    // Target Sum
+    public static int pairSum(int arr[], int target) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static int triplets(int arr[], int target) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++)
+                    if (arr[i] + arr[j] + arr[k] == target) {
+                        count++;
+                    }
+            }
+        }
+        return count;
+    }
+
+    public static ArrayList<Integer> uniqueElement(int arr[]) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int num : map.keySet()) {
+            if(map.get(num) == 1) {
+                result.add(num);
+            }
+        }
+
+        return result;
+    }
+
+    public static void secondLargest(int arr[]) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<arr.length; i++) {
+            set.add(arr[i]);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+
+        if(list.size() < 2) {
+            System.out.println("No second largest element");
+        } else {
+            System.out.println("Second Largest : " + list.get(list.size() - 2));
+        }
+    }
+
+    public static void main(String args[]) {
         // printPascalTriangle(5);
         // int numbers[] = {2, 4, 6, 8, 10};
         // System.out.println(getLargest(numbers));
@@ -269,8 +327,10 @@ public class ArrayCC {
         // System.out.println(binarySearch(numbers, 6));
         // pairArr(numbers);
         // subArray(numbers);
-        int arr[] = {1, 2, 3, 4, 5};
+        // int arr[] = {1, 2, 3, 4, 5};
         // System.out.println(Arrays.toString(prefixSum(arr)));
-        System.out.println(subarrayPartition(arr));
+        // System.out.println(subarrayPartition(arr));
+        int arr[] = { 9, 8, 9, 6, 9, 5, 8 };
+        secondLargest(arr);
     }
 }
